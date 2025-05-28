e2a806a4c86c4edfe629212e07bc09ef2ef421c5
tiara97 <tiara.hanindita@rds.co.id>
2025-05-28 10:37:14 +0700

    FIX: fix params

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -8,8 +8,13 @@ import { projects } from "@/lib/projects-data";
 import { notFound } from "next/navigation";
 import YouTubePlayer from "@/components/youtube-player";
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
-  const project = projects.find((p) => p.slug === params.slug);
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const project = projects.find((p) => p.slug === slug);
   if (!project) {
     notFound();
   }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: "export",  // <=== enables static exports
   eslint: {
     ignoreDuringBuilds: true,
   },
